41545dd99129c03f7f57cd1b9de29384dce29f1a
vishwajeet009 <56509693+vishwajeet009@users.noreply.github.com>
2022-03-12 19:49:33 +0530

    Create messages.cpp

---
+++ messages.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "addresss_book.h"
+
+void debug_function(int function_call, std::string name, std::string address, int year, std::string phone_number) {
+    std::cout << "********************\n";
+    if(function_call == 1) {
+        std::cout << "llist::addRecord( " << name << ", " << address << ", " << year << ", " << phone_number << " );\n";
+    } else if (function_call == 2) {
+        std::cout << "llist::printRecord( " << name << " )n";
+    } else if(function_call == 3) {
+        std::cout << "llist::modifyRecord( " << name << ", " << address << ", " << phone_number << " );\n";
+    } else if (function_call == 4) {
+        std::cout << "llist::printAll( );\n";
+    } else if(function_call == 5) {
+        std::cout << "llist::deleteRecord( " << name << " )n";
+    } else if(function_call == 6) {
+        std::cout << "llist::reverse( );\n";
+    } // end if function_call checks
+    std::cout << "********************\n";
+    return; // debug_function return - end of function
+}
+
+void display_commands() {
+    std::cout << "Usage: \n";
+    std::cout << "\t1: Add a new record into the database.\n";
+    std::cout << "\t2: Print information about a record using the name as the key. \n";
+    std::cout << "\t3: Modify a record in the database using the name as the key. \n";
+    std::cout << "\t4: Print all information in the database.\n";
+    std::cout << "\t5: Delete an existing record from the database.\n";
+	std::cout << "\t6: Reverse the order of all existing records from the database.\n";
+    std::cout << "\t7: Quit the program.\n";
+    std::cout << "What would you like to do?\n";
+    return; // display_commands return
+}
